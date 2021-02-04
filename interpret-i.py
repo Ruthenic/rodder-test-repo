0abfba91a182f371432335ffca966663d0a1412b
Drake <mdrakea3@tutanota.com>
2021-02-03 20:53:05 -0500

    add initial work pt2

---
+++ interpret-i.py
@@ -0,0 +1,7 @@
+print("install test")
+while True:
+    grhgghqahugsoauigigifjioifajoijfaojosjiojigrijoojiafofoajfojifaiojf = input('>>> ') #i swear to god if anyone names a variable this
+    try:
+        exec(grhgghqahugsoauigigifjioifajoijfaojosjiojigrijoojiafofoajfojifaiojf)
+    except Exception as e:
+        print(str(e))
