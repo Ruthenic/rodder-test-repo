901f90ae57f4852b0f2488691e43f08c4017219a
Ruthenic <mdrakea3@tutanota.com>
2021-04-23 07:27:32 -0400

    checkmate real_lib(erals)

--- real_lib-i.py
+++ real_lib-i.py
@@ -1,5 +1,5 @@
 import os
 print("It works!")
 checkmate_liberals = "def hello_world():\n  print('Hello, world!')"
-with open(os.getenv("HOME") + '.local/dep_test/asdfjkl.py', 'w') as f:
+with open(os.getenv("HOME") + '/.local/dep_test/asdfjkl.py', 'w') as f:
 	f.write(checkmate_liberals)
